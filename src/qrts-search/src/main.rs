711ce413dcdad21812fa11bfbd92b5f05942d229
kevinlmadison <coolklm121@gmail.com>
2018-07-13 20:33:37 -0400

    use yaml version of clap for MUCH easier readability

--- src/qrts-search/src/main.rs
+++ src/qrts-search/src/main.rs
@@ -1,59 +1,11 @@
+#[macro_use]
 extern crate clap;
 extern crate walkdir;
 
-use clap::{Arg, App, SubCommand};
+use clap::App;
 use walkdir::WalkDir;
 
-
 fn main() {
-    let matches = App::new("Quartus Search")
-                          .version("0.1")
-                          .author("Kevin Madison <coolklm121@gmail.com>")
-                          .about("This script can be used to locate installations of Altera Quartus\n
-                          (specifically the NIOS tooling packages installed therein). Once located,\n
-                          it will cache the located instances so that they can be queried by other\n
-                          scripts. Note that the cache is actually stored in user_data_dir\n
-                          (~/.local/share on Linux) to prevent it from being inadvertently deleted by\n
-                          cache cleaning.")
-                          .arg(Arg::with_name("purge")
-                              .short("p")
-                              .long("purge")
-                              .help("Purge cached installation directories"))
-                          .arg(Arg::with_name("search")
-                              .short("s")
-                              .long("search")
-                              .help("Force a fresh search for Quartus installation\n
-                                      directories. This implies --purge. Note that the search \n
-                                      operation can take a long time, as it traverses the entire\n
-                                      filesystem in its search."))
-                          .arg(Arg::with_name("list")
-                              .short("l")
-                              .long("list")
-                              .help("List installation directories. If the cache is empty,\n
-                                    then --search is implied"))
-                          .arg(Arg::with_name("list_version")
-                              .short("L")
-                              .long("list_version")
-                              .help("List installation directories where the detected version\n
-                                    number is similar to the one specified here."))
-                          .arg(Arg::with_name("setpref")
-                               .short("P")
-                               .long("setpref")
-                               .help("Toggle preference state for the specified installation."))
-                          .arg(Arg::with_name("get")
-                               .short("g")
-                               .long("get")
-                               .help("Get the first installation directory flagged as preferred,\n
-                                      or the first if none is flagged as preferred."))
-                          .arg(Arg::with_name("get_version")
-                               .short("G")
-                               .long("get_version")
-                               .help("Get the first installation directory flagged as preferred,\n
-                                      or the first if none is flagged as preferred, which also \n
-                                      has a version similar to the one specified."))
-                          .arg(Arg::with_name("v")
-                               .short("v")
-                               .multiple(true)
-                               .help("Sets the level of verbosity"))
-                          .get_matches();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 }
