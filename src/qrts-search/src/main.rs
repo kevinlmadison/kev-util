f64aa18ac37430c5db02351a5ee4021534e85e7d
kevinlmadison <coolklm121@gmail.com>
2018-07-13 22:01:33 -0400

    Add mutex group and other small details to yaml

--- src/qrts-search/src/main.rs
+++ src/qrts-search/src/main.rs
@@ -8,4 +8,7 @@ use walkdir::WalkDir;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+    if matches.is_present("verbose") {
+        println!("Verbose output enabled.");
+    }
 }
